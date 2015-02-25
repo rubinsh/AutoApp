af2d8fe18bd2ace23d6b08af602f5b4f154613c6
Shai Rubinstein <rubinsh@gmail.com>
2015-02-25 23:30:15 +0200

    changed main page carousel to have 6 articles

--- app/assets/javascripts/controllers/IndexCtrl.js
+++ app/assets/javascripts/controllers/IndexCtrl.js
@@ -4,32 +4,16 @@ angular.module('autoControllers')
               var stop;
               $scope.navs = NavServices.navs;
               $scope.currIndex = 0;
-              //stop = $interval(function() {
-                //$scope.currIndex++;
-                //$scope.currIndex = $scope.currIndex % ($scope.latest.length);
-              //},6000);
-
-              //$scope.stopCarousel = function() {
-                //if (angular.isDefined(stop)) {
-                  //$interval.cancel(stop);
-                  //stop = undefined;
-                //}
-              //};
-
               $scope.setIndex = function(index) {
-                //$scope.stopCarousel();
                 $scope.currIndex = index;
               };
 
-              //$scope.$on("$destroy", function() {
-                //$scope.stopCarousel();
-              //});
 
               var convertToLargeImage = function(imageUrl) {
                 return imageUrl.split(".jpg")[0]+"-4.jpg";
               };
 
-              $http.get(autoApiPrefix + 'articles/latest').success(function(data) {
+              $http.get(autoApiPrefix + 'articles/latest?limit=6').success(function(data) {
                 angular.forEach(data, function(item, index) {
                   data[index].thumbUrl = data[index].imageUrl;
                   data[index].imageUrl = convertToLargeImage(data[index].imageUrl);
