c8d3d9c272dc9a9d8310095a089e26208ec70e24
Shai Rubinstein <rubinsh@gmail.com>
2015-05-06 12:03:22 +0300

    small change

--- app/frontend/javascripts/controllers/IndexCtrl.js
+++ app/frontend/javascripts/controllers/IndexCtrl.js
@@ -1,7 +1,7 @@
 'use strict';
 angular.module('autoControllers')
-.controller('IndexCtrl', ['$scope', '$interval', '$templateCache', '$http', 'NavServices','CategoriesService','ArticlesServices',
-  function($scope, $interval, $templateCache, $http, NavServices,CategoriesService,ArticlesServices) {
+.controller('IndexCtrl', ['$scope', '$interval', '$templateCache', '$http','$q', 'NavServices','CategoriesService','ArticlesServices',
+  function($scope, $interval, $templateCache, $http, $q, NavServices,CategoriesService,ArticlesServices) {
     var stop;
 
     $scope.navs = NavServices.navs;
@@ -16,28 +16,23 @@ angular.module('autoControllers')
     };
 
     var articleServices = ArticlesServices;
+    var getData = function() {
+      var promises = [];
 
-    ArticlesServices.getAllArticles().success(function(data) {
-        $scope.articlesCategoriesCollection = {};
-        angular.forEach(ArticlesServices.categories, function(item, key) {
-            $scope.articlesCategoriesCollection[key] = {
-                categoryName: item,
-                categoryId: key,
-                articles: []
-            };
-        });
-        angular.forEach(data, function(item, index) {
-            $scope.articlesCategoriesCollection[item.categoryId].articles.push(item);
-        });
-        $scope.articlesList = []
-        angular.forEach($scope.articlesCategoriesCollection, function(item, index) {
-          $scope.articlesList.push(item);
-        });
-        $scope.articlesCategoriesList = null;
-    }).then(function() {
-      CategoriesService.getCategories()
+      var res = ArticlesServices.getAllArticles()
+      .success(function(data) {
+          console.dir(data);
+
+          $scope.articlesList = []
+          angular.forEach(data, function(item, index) {
+            $scope.articlesList.push(item);
+          });
+      });
+      promises.push(res);
+      
+      
+      res = CategoriesService.getCategories()
       .then(function(categories) {
-        console.log(categories);
         $http.get(autoApiPrefix + 'articles/latest?limit=4')
         .then(function(result) {
           var data = result.data;
@@ -47,10 +42,17 @@ angular.module('autoControllers')
             data[index].imageUrl = convertToLargeImage(data[index].imageUrl);
           });
           $scope.mainArticle = data.shift();
-          $scope.latest = data;
         });  
-      });  
+      });
+
+      promises.push(res);
+      return $q.all(promises);
+    };
+
+    getData().then(function() {
+      $scope.latest=true;
     });
+    
 
     
 
