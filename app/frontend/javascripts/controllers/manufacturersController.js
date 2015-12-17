50aef2ace39317d8d6698eaa543f75ea6ecd5fb5
rubinsh <rubinsh@gmail.com>
2015-12-17 23:14:01 +0200

    bug fixes, changed header and manf list

--- app/frontend/javascripts/controllers/manufacturersController.js
+++ app/frontend/javascripts/controllers/manufacturersController.js
@@ -2,16 +2,20 @@ angular.module('autoControllers')
     .controller('ManufacturersCtrl', ['$scope', '$location', 'ManufacturersServices', 'CatalogServices',
         function($scope, $location, ManufacturersServices, CatalogServices) {
             ManufacturersServices.getAllManufacturers().success(function(data) {
-                $scope.manufacturers = {};
-                angular.forEach(data, function(item, index) {
-                    if (!$scope.manufacturers[item.name.charAt(0)]) {
-                        $scope.manufacturers[item.name.charAt(0)] = {};
-                        $scope.manufacturers[item.name.charAt(0)].key = '';
-                        $scope.manufacturers[item.name.charAt(0)].collection = [];
-                    }
-                    $scope.manufacturers[item.name.charAt(0)].key = item.name.charAt(0);
-                    $scope.manufacturers[item.name.charAt(0)].collection.push(item);
-                });
+                $scope.manufacturers = [];
+                angular.forEach(data, function(item,index) {
+                  item.logoUrl += "?w=64";
+                  $scope.manufacturers.push(item);
+                })
+                // angular.forEach(data, function(item, index) {
+                //     if (!$scope.manufacturers[item.name.charAt(0)]) {
+                //         $scope.manufacturers[item.name.charAt(0)] = {};
+                //         $scope.manufacturers[item.name.charAt(0)].key = '';
+                //         $scope.manufacturers[item.name.charAt(0)].collection = [];
+                //     }
+                //     $scope.manufacturers[item.name.charAt(0)].key = item.name.charAt(0);
+                //     $scope.manufacturers[item.name.charAt(0)].collection.push(item);
+                // });
 
             });
 
